1f68070f30dd53a7389e2e66c1bc04944f5a614f
이경민 <lienero@naver.com>
2021-01-28 13:59:03 +0900

    0128 Laravel 계층형 게시판 라우팅 지정

--- demo_board/routes/web.php
+++ demo_board/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// routes/web.php 에 정의된 라우트는 브라우저를 통해서 유입되는 라우트 URL을 정의하는데 사용
+// Route::메소드('URL경로', '컨트롤러의 클래스명@메소드명'); 을 지정
+// 경로가 맞다면 TaskController 클래스의 index 메소드가 실행
+Route::get('/tasks/', 'TaskController@index');
+Route::get('/tasks/create', 'TaskController@create');
+Route::post('/tasks/', 'TaskController@store');
