1226a0fc4e582851e6327663bec27aeacf38b2d1
이경민 <lienero@naver.com>
2021-01-28 13:48:33 +0900

    0128 Laravel 계층형 게시판 주석작성

--- demo_board/database/migrations/2021_01_28_042635_create_tasks_table.php
+++ demo_board/database/migrations/2021_01_28_042635_create_tasks_table.php
@@ -15,6 +15,15 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('Task', function (Blueprint $table) {
+            /**
+             * id : 글 번호, 기본키입니다. increment 속성이므로 데이터가 생성되면 1씩 증가합니다.
+             * memo : 사용자가 작성한 글.
+             * creator_name : 사용자가 입력한 사용자의 이름입니다.
+             * grp : 같은 주제를 갖는 게시물의 고유번호. 부모 글과 부모 글로부터 파생된 모든 자식 글은 같은 번호를 갖습니다.
+             * sort : 같은 그룹 내 댓글의 순서.
+             * depth : 같은 그룹 내 댓글의 계층.
+             */
+            // $table->속성('컬럼명'); 의 형식으로 정의
             $table->increments('id');
             $table->text('memo');
             $table->text('creator_name');
