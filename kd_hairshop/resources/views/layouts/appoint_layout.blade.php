90e739f1193e48a62679c42520fc696f39a43ee6
이경민 <lienero@naver.com>
2021-01-31 13:47:39 +0900

    0131 라라벨 프로젝트 백업

---
+++ kd_hairshop/resources/views/layouts/appoint_layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    @yield('metatag')
+    <!-- 헤드 부분. -->
+    <title>@yield('title')</title> <!-- 타이틀 제목 -->
+    @yield('style')
+    <!-- 스타일 컨텐츠가 들어갈 부분 -->
+</head>
+
+<body>
+    @yield('content')
+    <!-- body 컨텐츠가 들어갈 부분 -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    @yield('script')
+    <!-- script가 들어갈 부분 -->
+</body>
+
+</html>
--- kd_hairshop/routes/web.php
+++ kd_hairshop/routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+// routes/web.php 에 정의된 라우트는 브라우저를 통해서 유입되는 라우트 URL을 정의하는데 사용
+// Route::메소드('URL경로', '컨트롤러의 클래스명@메소드명'); 을 지정
+// 경로가 맞다면 TaskController 클래스의 index 메소드가 실행
+
+use App\Http\Controllers\AppointController;
+
+Route::get('/appoint', [AppointController::class,'index']); 
+
+Route::get('/appoint/create',  [AppointController::class,'create']); 
+
+Route::post('/appoint',  [AppointController::class,'store']); 
