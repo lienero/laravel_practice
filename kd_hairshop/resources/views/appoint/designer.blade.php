64c14429fc16c1d557452f9a3659e1f71cc37e3a
이경민 <lienero@naver.com>
2021-02-02 23:15:44 +0900

    0202 디자이너&시간 선택 기능 버그 수정 및 백업

--- kd_hairshop/resources/views/appoint/designer.blade.php
+++ kd_hairshop/resources/views/appoint/designer.blade.php
@@ -9,10 +9,12 @@
     <br>
 </div>
 <?php 
+$staff_count = 0;
 for($i=1; $i<7; $i++) {
     $staff = 'staff_'.$i; 
     if($disigner->$staff != NULL) {
        echo '<div> <a href="/appoint/create?date='.$date.'&designer='.$staff.'">'.$staff.'</a></div>';
+       $staff_count++;
     }
 }
 ?>
@@ -29,15 +31,29 @@ foreach($appoints as $appoint){
 }
 for($k=0; $k < count($appoint_time); $k++){
     $count = 0;
+    # 디자이너 전원의 예약이 차있는지 확인하는 변수
+    $st_count = 0;
+    $end_count = 0;
+    for($l=0; $l<$j; $l++){
+        list($st_date, $st_time) = explode(" ",$appoint_st[$l]);
+        $strto_appoint_end[$l] = strtotime($appoint_end[$l].'-30 minutes');
+        $strto_appoint_end[$l] = date("Y-m-d h:i:s", $strto_appoint_end[$l]);
+        list($end_date, $end_time) = explode(" ", $strto_appoint_end[$l]);
+        if($st_time == $appoint_time[$k].':00'){
+            $st_count++;
+        }
+        if($end_time == $appoint_time[$k].':00' || $st_time == $appoint_time[$k].':00'){
+            $end_count++;
+        }
+    }
     for($l=0; $l<$j; $l++){
         list($st_date, $st_time) = explode(" ",$appoint_st[$l]);
         list($end_date, $end_time) = explode(" ",$appoint_end[$l]);
-        if($count == 0) {
+        if($count == 0 && $st_count < $staff_count && $end_count < $staff_count) {
             if($appoint_time[$k].':00' < $st_time || $appoint_time[$k].':00' >= $end_time){
-                
                 echo '&nbsp;&nbsp;<a href="/appoint/create?date='.$date.'&time='.$appoint_time[$k].':00">'.$appoint_time[$k].'</a>';
                 $count=1;
-            } 
+            }
         }
     }
     if(($k%5) == 0 && $k != 0) {
--- kd_hairshop/resources/views/layouts/footer.blade.php
+++ kd_hairshop/resources/views/layouts/footer.blade.php
@@ -1,4 +1,23 @@
 @section('footer')
 <body>
+    <br>
+    <div class="container mx-auto lg:flex justify-between items-center">
+        <div class="lg:w-6/12 lg:p-0 p-7">
+           <h1 class="text-4xl font-bold leading-tight mb-5 capitalize">  KD-hair. </h1>
+           <p class="text-2xl font-semibold">  00-0000-0000 </p>
+           <p class="text-xl font-semibold">  kd-hair@gmail.com</p>
+           <p class="text-xl font-semibold">  神田駅3丁目１２−2</p>
+           <p class="text-xl font-semibold">  休日：第三水曜日休みます。</p>
+           <div class="py-5">
+                <p href="#" class="text-white rounded-full py-2 px-5 text-lg font-semibold bg-purple-600 inline-block border border-purple-600 mr-3">Open : 09:00</p>
+                <p href="#" class="text-black rounded-full py-2 px-5 text-lg font-semibold bg-gray-400 inline-block border">Close : 22:00</p>
+           </div>
+ 
+         </div>
+         <div class="lg:w-5/12 order-2">
+           <img src="/img/kd-hair.png"
+           style="transform: scale(1) perspective(1040px) rotateY(-11deg) rotateX(2deg) rotate(2deg);" alt="" class="rounded">
+         </div>
+     </div>
 </body>
 @endsection
